82214de3bb41b6925ebbc4471784df8375abe175
watercereal <watercereal11@gmail.com>
2025-10-06 21:59:27 -0400

    move const definitions to header file, style changes, add bind, listen, and accept

---
+++ const.h
@@ -0,0 +1,12 @@
+#define SOCKET_CALL 102
+#define SYS_SOCKET 1
+#define SYS_BIND 2
+#define SYS_CONNECT 3
+#define SYS_LISTEN 4
+#define SYS_ACCEPT 5
+	
+#define AF_UNIX 1
+#define SOCK_STREAM 1	
+#define AF_INET 2
+#define INADDR_ANY 0
+	
--- gopher.s
+++ gopher.s
@@ -1,21 +1,27 @@
+#include "const.h"
 
-#define AF_UNIX 1
-#define SOCK_STREAM 1	
-#define AF_INET 2
-
-	
 	.section .data
 
 rec_buf_len:
 	.long 32
-#af_unix:			## socket domain for machine-local communication
-#	.long 1
-#af_inet:			## socket domain for ipv4
-#	.long 2
-#sock_stream:			## socket type 
-#	.long 1
 socket_fail_msg:
 	.ascii "socket creation failed: %d\n\0"
+socket_success_msg:
+	.ascii "created socket successfully\n\0"
+bind_fail_msg:
+	.ascii "bind failed: %d\n\0"
+bind_success_msg:
+	.ascii "bound socket successfully\n\0"
+listen_fail_msg:
+	.ascii "listen failed: %d\n\0"
+listen_success_msg:
+	.ascii "server listening\n\0"
+accept_fail_msg:
+	.ascii "accept failed: %d\n\0"
+accept_success_msg:
+	.ascii "server accepted\n\0"		
+	
+	
 	
 msg:
 	.ascii "the value of x is %d\n\0"
@@ -26,9 +32,9 @@ msg:
 
 
 	.type main, @function
-main:				#- main function
+main:				# main function
 	xor %edi, %edi
-1:				## write zeroed buffer to stack of length rec_buf_len
+1:				# write zeroed buffer to stack of length rec_buf_len
 	cmp rec_buf_len, %edi	
 	jge 1f
 	pushw $0		
@@ -36,34 +42,111 @@ main:				#- main function
 	inc %edi		
 	jmp 1b			
 1:
-	movl $0x167, %eax	## syscall socket ( family, type, protocol )
-	movl $AF_INET, %ebx	## int family
-	movl $SOCK_STREAM, %ecx	## int type
-	movl $0, %edx		## int protocol
-	int $0x80
+				#-# push arguments for socket socket
+				# >(int domain, int type, int protocol)
+	pushl $0		# int protocol
+	pushl $SOCK_STREAM	# int type 
+	pushl $AF_INET		# int domain
 	
-#	pushl $0		## int protocol
-#	pushl sock_stream	## int type 
-#	pushl af_inet		## int domain
-#	call socket		## call socket(af_inet, sock_stream, 0)
-#	addl $6, %esp		## clear arguments from stack
-	
-	cmpl $-1, %eax		## check for error
-	jle socket_fail_t
-	jmp socket_fail_f
-socket_fail_t:			## print socket_fail_msg and jump to exit_err
+	movl $SOCKET_CALL, %eax
+	movl $SYS_SOCKET, %ebx	
+	movl %esp, %ecx
+	int $0x80		# call socket ()
+	addl $12, %esp		# clear args from stack
+
+	cmpl $0x0, %eax		# check for error
+	jge socket_fail_f
+socket_fail_t:			# show socket fail message and quit
 	pushl %eax
 	pushl $socket_fail_msg
 	call printf
 	jmp exit_err
-socket_fail_f:
+socket_fail_f:			# socket didn't fail
+	movl %eax, %esi
+	pushl $socket_success_msg 
+	call printf
+	addl $4, %esp
 	
-	movl %eax, %ebx		## move return value of socket
-				 ## to the first argument of bind
+				#-# push sockaddr values to stack
+	pushl $0x0		# char sin_zero[8]
+	pushl $0x0		# ^
+	pushl $INADDR_ANY	# struct in_addr sin_addr
+	pushw $0x901f		# in_port_t sin_port : 0x46(70) in network byte order
+	pushw $AF_INET		# sa_family_t sin_family
+	movl %esp, %ebx
+				#-# push arguments for bind
+				# >(int sockfd, const struct sockaddr *addr, socklen_t addrlen) 
+	pushl $16		# sizeof struct sockaddr (16 bytes - 128 bits)
+	pushl %ebx		# pointer to sockaddr
+	pushl %esi		# sockfd (return value from socket())
+
+	movl $SOCKET_CALL, %eax		
+	movl $SYS_BIND, %ebx
+	movl %esp, %ecx	
+	int $0x80		# call bind()
+	addl $24, %esp
 	
-	movl $0x14b, %eax	## syscall
-	
-	
+
+	cmp $0x0, %eax
+	jge bind_fail_f
+bind_fail_t:			# show bind fail msg and quit
+	pushl %eax
+	pushl $bind_fail_msg
+	call printf
+	jmp exit_err
+bind_fail_f:			# bind didn't fail
+	pushl $bind_success_msg	
+	call printf
+	addl $4, %esp
+
+				#-# push arguments for listen
+				# >(int sockfd, int backlog)
+	pushl $5
+	pushl %esi
+
+	movl $SOCKET_CALL, %eax
+	movl $SYS_LISTEN, %ebx
+	movl %esp, %ecx
+	int $0x80		# call listen()
+
+	cmp $0x0, %eax
+	jge listen_fail_f
+listen_fail_t:			# show listen fail msg and quit
+	pushl %eax
+	pushl $listen_fail_msg
+	call printf
+	jmp exit_err
+listen_fail_f:			# listen didn't fail
+	pushl $listen_success_msg
+	call printf
+	addl $4, %esp
+
+	subl $0x10, %esp	# allocate space for sockaddr
+	movl %esp, %eax
+				#-# push arguments for accept
+				# >(int sockfd, struct sockaddr *_Nullable restrict addr, socklen_t *_Nullable restrict addrlen)
+	pushl $16
+	pushl %eax
+	pushl %esi
+
+	movl $SOCKET_CALL, %eax
+	movl $SYS_ACCEPT, %ebx
+	movl %esp, %ecx
+	int $0x80		# call accept()
+	addl $28, %esp
+
+	cmp $0x0, %eax
+	jge accept_fail_f
+accept_fail_t:			# show accept fail msg and quit
+	pushl %eax
+	pushl $accept_fail_msg
+	call printf
+	jmp exit_err
+accept_fail_f:			# accept didn't fail
+	pushl $accept_success_msg
+	call printf
+	addl $4, %esp
+
 	
 	
 	
